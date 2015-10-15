e09c9c5291a45984b59d48a7ed94c6b443c6c84c
carlsim66 <carlsim66@yahoo.com>
2015-10-15 16:23:30 -0700

    LEARNING PANCAKES! YAY!

---
+++ drive.h
@@ -0,0 +1,30 @@
+#include "Macros.h"
+//below you can create a parameters (int left, int right)and put all the motors you can set parameters to.
+//and also you can pacake the stuff so here im naming the motors and setting parameters to them
+void updateDriveMotors (left,right)
+{
+	motor[front_left_drive_motor] = left;
+	motor[back_left_drive_motor] = left;
+	motor[front_right_drive_motor] = right;
+	motor[back_right_drive_motor] = right;
+}
+//this will make it go back and forth! yay life
+//use the void function when you're not making a true or false statemnt or when you're "returning" something
+//
+void drive ()
+{
+	if (driveY > DEADZONE || driveY < -DEADZONE)
+	{
+		updateDriveMotors(driveY, driveY);
+	}
+//now for turning!
+
+	if (driveX > DEADZONE || driveX < -DEADZONE)
+	{
+	updateDriveMotors (driveX, -driveX);
+	}
+	else if
+	{
+		updateDriveMotors (0,0);
+	}
+}
